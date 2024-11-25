3862a5f256dfba21ce1779fda8cc8adf09c3b983
datmicw <dat.micf@gmail.com>
2024-11-25 10:30:03 +0700

    Continous Create Train and Prefabs

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;       // Đối tượng mà camera sẽ theo dõi
+    public Vector3 offset;         // Vị trí cố định của camera so với nhân vật
+    public float smoothSpeed = 0.125f; // Tốc độ di chuyển mượt mà
+
+    void LateUpdate()
+    {
+        // Target là nhân vật mà camera sẽ theo dõi
+        if (target != null)
+        {
+            // Vị trí camera (phía sau nhân vật)
+            Vector3 desiredPosition = target.position + offset;
+
+            // Di chuyển camera mượt mà
+            transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+            // Đảm bảo camera luôn nhìn về phía nhân vật
+            transform.LookAt(target);
+        }
+    }
+}
---
+++ Assets/Scripts/MapRun.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapRun : MonoBehaviour
+{
+    public float MapSpeed = 5.0f;
+
+    void Update()
+    {
+        // Di chuyển toàn bộ đoạn map
+        transform.Translate(Vector3.back * MapSpeed * Time.deltaTime);
+    }
+}
